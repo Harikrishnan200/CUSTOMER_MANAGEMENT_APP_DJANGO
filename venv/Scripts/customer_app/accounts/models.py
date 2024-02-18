f1abf6b6016ce092a1e2ac170f12eacc51d5bdcf
Harikrishnan <haripdm7994614917@gmail.com>
2024-02-18 11:44:41 +0530

    data models designed

--- venv/Scripts/customer_app/accounts/models.py
+++ venv/Scripts/customer_app/accounts/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+class  Customer(models.Model):
+    name = models.CharField(max_length=50,null=True)
+    phone= models.CharField(max_length=10,null=True)
+    email = models.CharField(max_length=50,null = True)
+    created_at = models.DateField(auto_now_add=True,null=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=50,null=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Product(models.Model):
+    CATEGORY = ( ('Indoor','Indoor'),
+                ('Out Door', 'Out Door'),
+                )
+    name = models.CharField(max_length=50,null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=50,null=True,choices=CATEGORY)
+    description = models.TextField()
+    created_at = models.DateField(auto_now_add=True,null=True)
+    tag = models.ManyToManyField(Tag)   #for multiple tags to one order
+
+    def __str__(self) -> str:
+        return self.name   
+
+
+
+class Orders(models.Model):
+    STATUS = (('Pending',"Pending"),
+              ("Out for Delivery","Out For Delivery")
+              ,("Delivered", "Delivered"))
+    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL, null=True, blank=True)
+    product = models.ForeignKey(Product,on_delete=models.SET_NULL, null=True, blank=True) 
+    created_at = models.DateField(auto_now_add=True,null=True)    
+    status = models.CharField(max_length=20, choices=STATUS, default='Pending')
+    
--- venv/Scripts/customer_app/customer_app/settings.py
+++ venv/Scripts/customer_app/customer_app/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'customer_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'customer_management_db',
+        'HOST':'localhost',
+        'PORT':'5432',
+        'USER':'postgres',
+        'PASSWORD':'postgres'
     }
 }
 
